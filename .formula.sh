f3b6e76f7260631ae6a3ba93a663e8ef3532104f
Chris Dzombak <chris@dzombak.com>
2024-05-30 08:58:39 -0400

    resolve formula style errors

--- .formula.sh
+++ .formula.sh
@@ -16,7 +16,7 @@ cat <<EOF
 
 # This file was automatically generated. DO NOT EDIT.
 class Cidrtool < Formula
-  desc "A simple command line tool to manipulate CIDR blocks"
+  desc "Simple command-line tool to manipulate CIDR blocks"
   homepage "https://github.com/cdzombak/cidrtool"
   url "https://github.com/cdzombak/cidrtool/releases/download/v${FORMULA_VERSION_NO_V}/cidrtool-${FORMULA_VERSION_NO_V}-all.tar.gz"
   sha256 "${FORMULA_TGZ_SHA256}"
